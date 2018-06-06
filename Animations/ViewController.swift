4c48f904ea2d9d09410bf0ff129d3a9bf790eeb1
Robert Jackson Jr <jacksor@bgsu.edu>
2018-06-05 21:58:18 -0400

    Complete Animations App.

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var image: UIImageView!
+    
+    var counter = 01
+    
+    
+    @IBAction func next(_ sender: Any) {
+    
+        image.image = UIImage(named: "frame_\(counter)_delay-0.07s.gif")
+        counter += 1
+        
+        if counter == 51 {
+            counter = 00;
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
